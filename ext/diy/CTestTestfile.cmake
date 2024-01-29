f86eec9bb90822e30c8d5fc24268f10e9fbeae4f
David Lenz <david.c.lenz.1@gmail.com>
2024-01-29 11:07:02 -0600

    Adds external dependencies

---
+++ ext/diy/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dlenz/workspace/mfa/moab-example/ext/diy
+# Build directory: /home/dlenz/workspace/mfa/moab-example/ext/diy
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
