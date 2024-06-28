8f4f2dda93f104a4f88f365acfa4d68ae2ee2706
David Lenz <david.c.lenz.1@gmail.com>
2024-06-28 09:22:25 -0500

    Starts to streamline remapping code

--- src/remap.cpp
+++ src/remap.cpp
@@ -54,7 +54,6 @@ int main(int argc, char** argv)
     int         geom_degree     = 1;        // degree for geometry (same for all dims)
     int         vars_degree     = 4;        // degree for science variables (same for all dims)
     int         ndomp           = 0;      // input number of domain points (same for all dims)
-    int         ntest           = 0;        // number of input test points in each dim for analytical error tests
     int         geom_nctrl      = -1;       // input number of control points for geometry (same for all dims)
     vector<int> vars_nctrl      = {11};     // initial # control points for all science variables (default same for all dims)
     string      input           = "sinc";   // input dataset
@@ -63,7 +62,6 @@ int main(int argc, char** argv)
     real_t      regularization  = 0;        // smoothing parameter for models with non-uniform input density (0 == no smoothing)
     int         reg1and2        = 0;        // flag for regularizer: 0 = regularize only 2nd derivs. 1 = regularize 1st and 2nd
     int         verbose         = 1;        // MFA verbosity (0 = no extra output)
-    int         weighted        = 0;        // Use NURBS weights (0/1)
     int         tot_blocks      = 1;        // 
     int         adaptive        = 0;        // do analytical encode (0/1)
     real_t      e_threshold     = 1e-1;     // error threshold for adaptive fitting
@@ -80,7 +78,6 @@ int main(int argc, char** argv)
     ops >> opts::Option('p', "geom_degree", geom_degree," degree in each dimension of geometry");
     ops >> opts::Option('q', "vars_degree", vars_degree," degree in each dimension of science variables");
     ops >> opts::Option('n', "ndomp",       ndomp,      " number of input points in each dimension of domain");
-    ops >> opts::Option('a', "ntest",       ntest,      " number of test points in each dimension of domain (for analytical error calculation)");
     ops >> opts::Option('g', "geom_nctrl",  geom_nctrl, " number of control points in each dimension of geometry");
     ops >> opts::Option('v', "vars_nctrl",  vars_nctrl, " number of control points in each dimension of all science variables");
     ops >> opts::Option('i', "input",       input,      " input dataset");
@@ -103,41 +100,8 @@ int main(int argc, char** argv)
     // print input arguments
     echo_mfa_settings("simple remap test", dom_dim, pt_dim, scalar, geom_degree, geom_nctrl, vars_degree, vars_nctrl,
                         regularization, reg1and2, adaptive, e_threshold, rounds);
-    echo_data_settings(input, "", 0, 0);
-
-    // ---------- debug: test of reading files --------------
-    // TODO: REMOVE once I know how to run the actual remap code
-
-    std::string mpas_infile = "mpas_outfile.h5m";
-    std::string roms_infile = "roms_outfile.h5m";
-    std::string read_opts   = "PARALLEL=READ_PART;PARTITION=PARALLEL_PARTITION;PARALLEL_RESOLVE_SHARED_ENTS;DEBUG_IO=3;";
-    ErrorCode   rval;
-
-    // initialize moab for mpas file
-    Interface*              mpas_mbi = new Core();                              // moab interface
-    ParallelComm*           mpas_pc  = new ParallelComm(mpas_mbi, local);     // moab communicator
-    EntityHandle            mpas_root;
-    rval = mpas_mbi->create_meshset(MESHSET_SET, mpas_root); ERR(rval);
-
-    // initialize moab for roms file
-    Interface*              roms_mbi = new Core();                              // moab interface
-    ParallelComm*           roms_pc  = new ParallelComm(roms_mbi, local);     // moab communicator
-    EntityHandle            roms_root;
-    rval = roms_mbi->create_meshset(MESHSET_SET, roms_root); ERR(rval);
-
-    // debug
-    fmt::print(stderr, "*** consumer before reading files ***\n");
-
-    // read files
-    rval = mpas_mbi->load_file(mpas_infile.c_str(), &mpas_root, read_opts.c_str() ); ERR(rval);
-    rval = roms_mbi->load_file(roms_infile.c_str(), &roms_root, read_opts.c_str() ); ERR(rval);
-
-    // debug
-    fmt::print(stderr, "*** consumer after reading files ***\n");
-
-    return 0;
-
-    // ------------- end of debug: test of reading files
+    fmt::print("MPAS file name: {}\n", mpasfile);
+    fmt::print("ROMS file name: {}\n", romsfile);
 
     // initialize DIY
     diy::FileStorage          storage("./DIY.XXXXXX"); // used for blocks to be moved out of core
@@ -165,14 +129,35 @@ int main(int argc, char** argv)
     // We assume that dom_dim == geom_dim
     // Different examples can reset this below
     vector<int> model_dims = {dom_dim, pt_dim - dom_dim};
-
+    if (dom_dim == 2)
+    {
+        model_dims = {3, 1, 1, 1};
+    }
+    else if (dom_dim == 3)
+    {
+        model_dims = {3, 1, 1};
+    }
+    else
+    {
+        cerr << "ERROR: Incorrect dom_dim" << endl;
+        exit(1);
+    }
 
     // set up parameters for examples
     MFAInfo     mfa_info(dom_dim, verbose);
-    DomainArgs  d_args(dom_dim, model_dims);
-    setup_args(dom_dim, pt_dim, model_dims, geom_degree, geom_nctrl, vars_degree, vars_nctrl,
-                input, "", ndomp, structured, rand_seed, 0, 0, 0,
-                reg1and2, regularization, adaptive, verbose, mfa_info, d_args);
+    
+    // If only one value for vars_nctrl was parsed, assume it applies to all dims
+    if (vars_nctrl.size() == 1)
+    {
+        vars_nctrl = vector<int>(dom_dim, vars_nctrl[0]);
+    }
+
+    set_mfa_info(dom_dim, model_dims, geom_degree, geom_nctrl, vars_degree, vars_nctrl, mfa_info);
+    mfa_info.verbose          = verbose;
+    mfa_info.weighted         = 0;
+    mfa_info.regularization   = regularization;
+    mfa_info.reg1and2         = reg1and2;
+
 
     double encode_time = MPI_Wtime();
     if (input=="roms")
